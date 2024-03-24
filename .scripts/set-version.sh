f490e45e801065cb410ab6d59b6b428243cc8479
Jeremy Beier <jeremy.beier@live.de>
2024-03-25 00:23:13 +0100

    remove unused script

--- .scripts/set-version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-new_version=$1
-sed -i "s/config\/version=\"[a-zA-Z0-9_\.\-]*\"/config\/version=\"${new_version}\"/g" project.godot
-sed -i "s/version=\"[a-zA-Z0-9_\.\-]*\"/version=\"${new_version}\"/g" addons/j_test/plugin.cfg
